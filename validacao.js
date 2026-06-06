801861763871c52ab73dd5e29f60893c57cb3a12
AlineAgui4r <alinehonoriodeaguiar@gmail.com>
2026-06-06 23:45:23 +0000

    Validação

---
+++ validacao.js
@@ -0,0 +1,24 @@
+form.addEventListener("submit", function(event) {
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const campoIdade = document.getElementById("campoIdade").value;
+
+    if (nome.trim() === "") {
+        event.preventDefault();  
+        erro.textContent = "O nome é obrigatório!";
+        erro.style.color = "red";
+    }
+
+    if (!email.includes("@")) {
+        event.preventDefault();
+        erro.textContent = "E-mail inválido!";
+
+}
+    if (campoIdade >= 0); 
+    event.preventDefault();
+    erro.textContent = "Idade invalida!";
+    
+    
+<script src="validacao.html"></script>
+
+});
